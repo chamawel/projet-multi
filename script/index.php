713dfb7e28464d6b70ae4db38e5f5d981fca9877
chamawel <ledentmael@gmail.com>
2023-10-11 19:08:57 +0000

    essai conection

--- script/index.php
+++ script/index.php
@@ -4,5 +4,12 @@
     $pswd         = "VP113"    ;
     $nameOfDB     = "rpg-297"  ;
 
+    $connexion    = new mysqli($nameOfServer,$login,$pswd);
+
+    if($connexion -> connect_error){
+        die("Erreur : " .$connexion -> connect_error);
+    
+    }
+    echo "connection error";
 
 ?>
