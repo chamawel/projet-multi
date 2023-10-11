44b33bf37a3502aee16f2f095fee5a973a8e1a0d
chamawel <ledentmael@gmail.com>
2023-10-11 20:02:22 +0000

    added cool stuff

--- script/index.php
+++ script/index.php
@@ -1,14 +1,24 @@
 <?php  
+    
+
     $nameOfServer = "localhost";
-    $login        = "fivett207";
+    $login        = "fivett207";    
     $pswd         = "VP113"    ;
-    $nameOfDB     = "rpg-297"  ;
+    $nameOfDB     = "rpg-5tt297"  ;
 
-    $connexion    = mysqli_connect($nameOfServer,$login,$pswd,$nameOfDB);
-    $message      = mysqli_query($connexion,"SELECT 'nom' FROM 'personnages' ");
-    $row          = mysqli_fetch_assoc($message);
 
-    echo $row['_msg'];
 
-    echo "BOO"
+    $id = mysqli_connect($nameOfServer,$login,$pswd,$nameOfDB) or die("Erreur de Connexion") ;
+
+    $res= mysqli_query($id, "select idPersonnage , nom , level from personnages" );
+
+    echo mysqli_num_rows($res)." - Ligne de la table :" ;
+    echo "<hr />";
+
+    while ($tab = mysqli_fetch_assoc($res)){
+        echo implode(" | ",$tab);
+        echo "<br />";
+    }
+   
+
 ?>
