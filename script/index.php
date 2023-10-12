4fa962fb511097927c77ff8b71edd568dfb9df7c
chamawel <ledentmael@gmail.com>
2023-10-12 17:12:14 +0000

    added a form

--- script/index.php
+++ script/index.php
@@ -1,6 +1,6 @@
 <?php  
     
-
+    
     $nameOfServer = "localhost";
     $login        = "fivett207";    
     $pswd         = "VP113"    ;
@@ -8,19 +8,41 @@
 
 
 
-    $id = mysqli_connect($nameOfServer,$login,$pswd,$nameOfDB) or die("Erreur de Connexion") ;
+    if( $_POST["login"] == $login and $_POST["password"] == $pswd ){
+        $id = mysqli_connect($nameOfServer,$login,$pswd,$nameOfDB) or die("Erreur de Connexion") ;
 
-    $res= mysqli_query($id, "select idPersonnage , nom , level from personnages" );
+        $res= mysqli_query($id, "select idPersonnage , nom , level from personnages" );
 
-    echo mysqli_num_rows($res)." - Ligne dans la table :" ;
-    echo "<hr />";
+        echo mysqli_num_rows($res)." - Ligne dans la table :" ;
+        echo "<hr />";
 
-    echo " <p><b> ID | Nom | Level </b> </p>   ";
+        echo " <p><b> ID | Nom | Level </b> </p>   ";
 
-    while ($tab = mysqli_fetch_assoc($res)){
-        echo implode(" | ",$tab);
-        echo "<br />";
+        while ($tab = mysqli_fetch_assoc($res)){
+                echo implode(" | ",$tab);
+                echo "<br />";
+        }  
+
     }
-   
+    else{
+        echo "Erreur De Connexion <br />";
+        echo "<a href='../formulaire-test.html'> <button type='button'> Retour </button> <a/> ";
+    }
+
+
+    // $id = mysqli_connect($nameOfServer,$login,$pswd,$nameOfDB) or die("Erreur de Connexion") ;
+
+    // $res= mysqli_query($id, "select idPersonnage , nom , level from personnages" );
+
+    // echo mysqli_num_rows($res)." - Ligne dans la table :" ;
+    // echo "<hr />";
+
+    // echo " <p><b> ID | Nom | Level </b> </p>   ";
+
+    // while ($tab = mysqli_fetch_assoc($res)){
+    //         echo implode(" | ",$tab);
+    //         echo "<br />";
+    // }
+    
 
 ?>
