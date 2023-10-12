fc40c07c96bd5d84370d5110446cbc7cc27de10f
chamawel <ledentmael@gmail.com>
2023-10-12 17:56:50 +0000

    made da form prettier

--- script/index.php
+++ script/index.php
@@ -9,7 +9,7 @@
 
 
     if( $_POST["login"] == $login and $_POST["password"] == $pswd ){
-        $id = mysqli_connect($nameOfServer,$login,$pswd,$nameOfDB) or die("Erreur de Connexion") ;
+        $id = mysqli_connect($nameOfServer,$login,$pswd,$nameOfDB) or die("Info de connexion incorrete") ;
 
         $res= mysqli_query($id, "select idPersonnage , nom , level from personnages" );
 
@@ -30,21 +30,5 @@
         echo "Erreur De Connexion <br />";
         echo "<a href='../formulaire-test.html'> <button type='button'> Retour </button> <a/> ";
     }
-
-
-    // $id = mysqli_connect($nameOfServer,$login,$pswd,$nameOfDB) or die("Erreur de Connexion") ;
-
-    // $res= mysqli_query($id, "select idPersonnage , nom , level from personnages" );
-
-    // echo mysqli_num_rows($res)." - Ligne dans la table :" ;
-    // echo "<hr />";
-
-    // echo " <p><b> ID | Nom | Level </b> </p>   ";
-
-    // while ($tab = mysqli_fetch_assoc($res)){
-    //         echo implode(" | ",$tab);
-    //         echo "<br />";
-    // }
-    
 
 ?>
